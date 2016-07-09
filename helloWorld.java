41147608ca0cbada437ab0cc47d22b4463788b2c
auwanogho <wanoxt@gmail.com>
2016-07-09 12:33:07 +0100

    added V2 to test Jenkins

--- helloWorld.java
+++ helloWorld.java
@@ -4,6 +4,6 @@
  */
 public class helloWorld { 
    public static void main(String[] args) { 
-      System.out.println("Hello, Automic World");
+      System.out.println("Hello, V2 Automic World");
    }
 }
