244f4fc2b4a9ff3b1a228c3543bbac4a43dddca6
auwanogho <wanoxt@gmail.com>
2016-07-09 20:32:03 +0100

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -4,6 +4,6 @@
  */
 public class helloWorld { 
    public static void main(String[] args) { 
-      System.out.println("Hello, webhook-V6 Automic World");
+      System.out.println("Hello, webhook-V7 Automic World");
    }
 }
