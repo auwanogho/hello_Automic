bf7cfcbf7fe11ddb4726e1dbd438fc529c263cb8
auwanogho <wanoxt@gmail.com>
2016-07-10 18:47:23 +0100

    mercy4

--- helloWorld.java
+++ helloWorld.java
@@ -4,6 +4,6 @@
  */
 public class helloWorld { 
    public static void main(String[] args) { 
-      System.out.println("Hello, Have mercy23 Lord");
+      System.out.println("Hello, Have mercy4 Lord");
    }
 }
