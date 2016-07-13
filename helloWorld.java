13b9ed0bb77ebebf33c55078b5476b55b1d5e862
auwanogho <wanoxt@gmail.com>
2016-07-13 01:12:11 +0100

    mercy 5

--- helloWorld.java
+++ helloWorld.java
@@ -4,6 +4,6 @@
  */
 public class helloWorld { 
    public static void main(String[] args) { 
-      System.out.println("Hello, new mercy4");
+      System.out.println("Hello, new mercy5");
    }
 }
